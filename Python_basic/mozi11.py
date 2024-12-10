7f5447404d0761f2fc660a61edb764d26d03b026
MaroMan1234 <naokippi1108@icloud.com>
2024-12-10 13:24:13 +0900

    文字列

---
+++ Python_basic/mozi11.py
@@ -0,0 +1,16 @@
+print("#################")
+print("""\
+line1
+line2
+line3""")
+print("#################")
+
+print("Hi." * 3 + "naoki")
+print("Py" + "thon")
+print("Py""thon")
+text = 'Py'
+print(text + 'thon')
+a = ("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+     "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB")
+print(a)
+print('test')
