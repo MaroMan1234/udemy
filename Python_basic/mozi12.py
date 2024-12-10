2f01929da5d51af52ea5dedfcfb3c1f24bcb60e7
MaroMan1234 <naokippi1108@icloud.com>
2024-12-10 13:24:36 +0900

    文字列のインデックスとスライス

---
+++ Python_basic/mozi12.py
@@ -0,0 +1,14 @@
+text = "Python"
+print(text[0])
+print(text[1])
+print(text[-1])
+print(text[0:2])
+print(text[2:5])
+print(text[:2])
+print(text[2:])
+word = 'j' + text[1:]
+print(word)
+n = len(word)
+#lenで字数を数えることができる
+
+print(n)
